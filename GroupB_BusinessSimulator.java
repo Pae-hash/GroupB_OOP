8e4545578be147a11d44b84a1add7e02143b58b3
Pae-hash <pedsont02@gmail.com>
2026-08-28 15:02:28 +0300

    This will display the discount or no discount comment

--- GroupB_BusinessSimulator.java
+++ GroupB_BusinessSimulator.java
@@ -17,3 +17,20 @@ public class GroupB_BusinessSimulator {
 
         return subtotal;
     }
+    
+    // This method creates the discount explanation that will be displayed next to each item on the receipt.
+    public static String buildNote(int quantity, int threshold, boolean neverDiscounted,
+                                    boolean isPercentage, double discountValue) {
+        if (neverDiscounted) {
+            return "no discount \u2014 never discounted";
+        }
+        if (quantity >= threshold) {
+            if (isPercentage) {
+                return (int) Math.round(discountValue * 100) + "% discount applied";
+            } else {
+                return "UGX " + String.format("%.0f", discountValue) + " off applied";
+            }
+        } else {
+            return "no discount \u2014 fewer than " + threshold;
+        }
+    }
